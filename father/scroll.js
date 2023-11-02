4313ffe5d017305e156c2638057f3cd41a276af7
OPDhaker <OPDhaker2007@gmail.com>
2023-11-03 05:13:06 +0530

    REACT UPDATE!

---
+++ father/scroll.js
@@ -0,0 +1,24 @@
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById('progress');
+    let progressValue = document.getElementById('progress-value');
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100)/calcHeight);
+    
+    if(pos > 100){
+        scrollProgress.style.display = 'grid';
+    }
+    else {
+        scrollProgress.style.display = 'none';
+    }
+
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+
+    scrollProgress.style.background = `conic-gradient(#00ff00 ${scrollValue}%, #1a1a1a ${scrollValue}%)`;
+
+}
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
